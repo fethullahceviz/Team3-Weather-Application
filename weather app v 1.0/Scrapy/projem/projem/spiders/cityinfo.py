677354a4a3ed2fb129a8f7f5d0da49a210f7d058
Fethullah Ceviz <96957283+fethullahceviz@users.noreply.github.com>
2022-06-02 22:15:08 +0200

    Update cityinfo.py

--- weather app v 1.0/Scrapy/projem/projem/spiders/cityinfo.py
+++ weather app v 1.0/Scrapy/projem/projem/spiders/cityinfo.py
@@ -5,7 +5,7 @@ from scrapy.crawler import CrawlerRunner
 import psycopg2
 import scrapy
 
-connection  = psycopg2.connect(host='localhost', user='postgres', password='123456', dbname='weather')
+connection  = psycopg2.connect(host='localhost', user='postgres', password=#enterpass, dbname=#enterdabasename)
 cur         = connection.cursor()
 
 cur.execute(""" CREATE TABLE IF NOT EXISTS city_info ( 
